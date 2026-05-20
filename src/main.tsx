e8dc7a17eb7eaf58233ad2354943c4405f0f2638
Titas <titas.sarkar420@hotmail.com>
2026-05-21 02:13:04 +0530

    Analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
@@ -25,5 +26,6 @@ createRoot(document.getElementById("root")!).render(
         {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       </QueryClientProvider>
     </BrowserRouter>
+    <Analytics />
   </StrictMode>,
 );
